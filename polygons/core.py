617bf9738da468a1b381ce0bca6c5a531ccf7083
ilyaabramovich <ilyaabramovich1998@gmail.com>
2020-03-26 16:59:08 +0300

    refactor visualization

--- polygons/core.py
+++ polygons/core.py
@@ -4,6 +4,7 @@ from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 import matplotlib.pyplot as plt
 from pathlib import Path
+import numpy as np
 from os import path
 import sys
 
@@ -26,16 +27,18 @@ def main(args):
     coords = get_polygon_coords(dataframe, inverse=True)
     new_polygon, split_line = split_polygon(coords)
     start, stop = split_line
-    transformed_coords = transform_coordinates(new_polygon, start, stop)
+    new_coords = transform_coordinates(new_polygon, start, stop)
     start_index = new_polygon.index(start)
     stop_index = new_polygon.index(stop)
+    new_split_line = [new_coords[start_index],
+                      new_coords[stop_index]]
+    new_start, new_stop = new_split_line
 
-    x0, y0 = transformed_coords[start_index]
-    x_1, y_1 = transformed_coords[stop_index]
-    x_a, y_a = transformed_coords[start_index-1]
-    x_b, y_b = transformed_coords[start_index+1]
-    x_c, y_c = transformed_coords[stop_index-1]
-    x_d, y_d = transformed_coords[stop_index+1]
+    x_1 = new_stop[0]
+    x_a, y_a = new_coords[start_index-1]
+    x_b, y_b = new_coords[start_index+1]
+    x_c, y_c = new_coords[stop_index-1]
+    x_d, y_d = new_coords[stop_index+1]
 
     h = (((-2*x_1*y_a*y_b*y_c+2*x_1*y_a*y_b*y_d-24*x_a*y_b*y_c+24*x_a*y_b*y_d+2*x_c*y_a*y_b*y_d+24*x_c*y_a*y_b-2*x_d*y_a*y_b*y_c-24*x_d*y_a*y_b)**2
           - 4*(ROAD_WIDTH*x_1*y_a*y_b*y_c-ROAD_WIDTH*x_1*y_a*y_b*y_d+144*x_a*y_b*y_c-144*x_a*y_b*y_d-ROAD_WIDTH*x_c*y_a*y_b*y_d-144*x_c*y_a*y_b+ROAD_WIDTH*x_d*y_a*y_b*y_c
@@ -55,7 +58,7 @@ def main(args):
 
     mid_top = (x_3+x_2)/2
     top_polygon = [[mid_top, h], [x_2, h], *
-                   transformed_coords[start_index+1:stop_index], [x_3, h]]
+                   new_coords[start_index+1:stop_index], [x_3, h]]
     new_polygon_top, split_line_top = split_polygon(top_polygon)
     new_polygon_top_1 = new_polygon_top[:new_polygon_top.index(
         split_line_top[1])+1]
@@ -64,65 +67,43 @@ def main(args):
 
     mid_bottom = (x_5+x_4)/2
     bottom_polygon = [[mid_bottom, h-ROAD_WIDTH], [x_4, h-ROAD_WIDTH], *
-                      transformed_coords[stop_index+1:], [x_5, h-ROAD_WIDTH]]
+                      new_coords[stop_index+1:], [x_5, h-ROAD_WIDTH]]
     new_polygon_bottom, split_line_bottom = split_polygon(bottom_polygon)
     new_polygon_bottom_1 = new_polygon_bottom[:new_polygon_bottom.index(
         split_line_bottom[1])+1]
     new_polygon_bottom_2 = [*new_polygon_bottom[new_polygon_bottom.index(
         split_line_bottom[1]):], new_polygon_bottom[0]]
 
-    road_top = [[x_3, h], transformed_coords[stop_index],
-                transformed_coords[start_index], [x_2, h]]
-    road_bottom = [transformed_coords[stop_index], [x_4, h-ROAD_WIDTH],
-                   [x_5, h-ROAD_WIDTH], transformed_coords[start_index]]
+    road_top = [[x_3, h], new_stop,
+                new_start, [x_2, h]]
+    road_bottom = [new_stop, [x_4, h-ROAD_WIDTH],
+                   [x_5, h-ROAD_WIDTH], new_start]
 
-    result_polygons = [new_polygon_top_1, new_polygon_top_2,
-                       new_polygon_bottom_1, new_polygon_bottom_1, road_top, road_bottom]
+    result_polygons = [new_coords, new_polygon_top_1, new_polygon_top_2,
+                       new_polygon_bottom_1, new_polygon_bottom_2, road_top, road_bottom]
 
     result_area = sum(
-        map(lambda polygon: area_by_shoelace(polygon), result_polygons))
-    # print("initial total area: {}\nresult total area: {}".format(area_by_shoelace(coords), result_area)
-    #       )
-
-    x_6 = split_line_top[1]
-    x_7 = split_line_bottom[1]
+        map(lambda polygon: area_by_shoelace(polygon), result_polygons[1:]))
+    print("initial total area: {}\nresult total area: {}".format(area_by_shoelace(coords), result_area)
+          )
 
     cut_points = [top_polygon[0], bottom_polygon[0],
                   split_line_bottom[1], split_line_top[1], [x_2, h], [x_3, h], [x_4, h-ROAD_WIDTH], [x_5, h-ROAD_WIDTH]]
-
+    colors = ["grey", "green", "red", "cyan", "magenta"]
+    patches = []
+    for coords in result_polygons[:-2]:
+        polygon = Polygon(coords)
+        patches.append(polygon)
+    p = PatchCollection(patches, fc="None", ec=colors)
     fig, ax = plt.subplots(2)
     polygon = Polygon(new_polygon)
-    p = PatchCollection([polygon], fc="none", ec="grey")
-    ax[0].add_collection(p)
-    x, y = zip(*new_polygon)
+    p1 = PatchCollection([polygon], fc="none", ec="grey")
+    ax[0].add_collection(p1)
     ax[0].axis("equal")
-    ax[0].set_xlim(min(x)-10, max(x)+10)
-    ax[0].set_ylim(min(y)-10, max(y)+10)
+    ax[0].scatter(*zip(*new_polygon))
     ax[0].plot(*zip(*split_line), "--")
-    ax[1].plot(*zip(*split_line_top), "--")
-    ax[1].plot(*zip(*split_line_bottom), "--")
-    ax[0].scatter(x, y, c="blue")
-    polygon = Polygon(transformed_coords)
-    p = PatchCollection([polygon], fc="none", ec="grey")
-    ax[1].add_collection(p)
-    x, y = zip(*transformed_coords)
     ax[1].axis("equal")
-    split_line = [transformed_coords[start_index],
-                  transformed_coords[stop_index]]
-    ax[1].plot(*zip(*split_line), "--")
-    ax[1].scatter(x, y, c="blue")
-    polygon = Polygon(new_polygon_top_1)
-    p = PatchCollection([polygon], fc="none", ec="green")
+    ax[1].plot(*zip(*new_split_line), "--")
     ax[1].add_collection(p)
-    polygon = Polygon(new_polygon_top_2)
-    p = PatchCollection([polygon], fc="none", ec="red")
-    ax[1].add_collection(p)
-    polygon = Polygon(new_polygon_bottom_1)
-    p = PatchCollection([polygon], fc="none", ec="cyan")
-    ax[1].add_collection(p)
-    polygon = Polygon(new_polygon_bottom_2)
-    p = PatchCollection([polygon], fc="none", ec="magenta")
-    ax[1].add_collection(p)
-    x, y = zip(*cut_points)
-    ax[1].scatter(x, y, c="orange")
+    ax[1].scatter(*zip(*cut_points))
     plt.show()
