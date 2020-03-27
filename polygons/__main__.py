2f00d8fe99f15ccec2dede03c0c276ee1bb5dc1c
ilyaabramovich <ilyaabramovich1998@gmail.com>
2020-03-27 23:46:08 +0300

    update arguments description

--- polygons/__main__.py
+++ polygons/__main__.py
@@ -2,7 +2,7 @@ from .core import main
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Split polygons.')
+parser = argparse.ArgumentParser(description='Split polygon into four equal parts considering road')
 parser.add_argument('filename', metavar="file.csv", type=argparse.FileType('r'),
                     help='Coordinates of polygon to split')
 parser.add_argument('--width', metavar="road_width", type=int,
